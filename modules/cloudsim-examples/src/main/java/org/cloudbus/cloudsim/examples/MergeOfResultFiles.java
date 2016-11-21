b1fc306c25770e545f336be026a019c9c687c9d6
alessandroliafook <alessandro.liafook@gmail.com>
2016-11-21 14:44:31 -0300

    Refactoring methods

--- modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/MergeOfResultFiles.java
+++ modules/cloudsim-examples/src/main/java/org/cloudbus/cloudsim/examples/MergeOfResultFiles.java
@@ -1,6 +1,5 @@
 package org.cloudbus.cloudsim.examples;
 import java.sql.SQLException;
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -15,7 +14,7 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
 /**
- * Created by jvmafra on 07/11/16.
+ * Created by Joao Victor Mafra and Alessandro Lia Fook Santos on 07/11/16.
  */
 public class MergeOfResultFiles {
 
@@ -25,13 +24,11 @@ public class MergeOfResultFiles {
     public static void main(String[] args) throws Exception{
 
         JCommander jc = new JCommander();
-
         executeCommand(jc, args);
-
-
     }
 
     public static void executeCommand(JCommander jc, String[] args) throws Exception{
+
         Command task = new Command();
         jc.addCommand("task", task);
 
@@ -44,7 +41,6 @@ public class MergeOfResultFiles {
         Command conclude = new Command();
         jc.addCommand("conclude", conclude);
 
-
         try {
             jc.parse(args);
         } catch (Exception e) {
@@ -52,6 +48,7 @@ public class MergeOfResultFiles {
             jc.usage();
             return;
         }
+
         String parsedCommand = jc.getParsedCommand();
 
         if (parsedCommand == null) {
@@ -62,6 +59,7 @@ public class MergeOfResultFiles {
         properties = new Properties();
 
         if (parsedCommand.equals("task")){
+
             double time = Double.parseDouble(task.time);
 
             properties.setProperty(TaskDataStore.DATABASE_URL_PROP, task.path_before);
@@ -77,11 +75,10 @@ public class MergeOfResultFiles {
             listOfAllTasks.addAll(tasks_after);
 
             properties.setProperty(TaskDataStore.DATABASE_URL_PROP, task.path_output);
-
 			storeTasks(listOfAllTasks);
 
-
         } else if (parsedCommand.equals("utilization")){
+
             double time = Double.parseDouble(utilization.time);
 
             properties.setProperty(HostUsageDataStore.DATABASE_URL_PROP, utilization.path_before);
@@ -98,6 +95,7 @@ public class MergeOfResultFiles {
 			loadAndStoreUsageEntries(time, inputDataStore.getMaxTraceTime(), inputDataStore, outputDataStore);
 
         } else if (parsedCommand.equals("datacenter")){
+
             double time = Double.parseDouble(datacenter.time);
 
             properties.setProperty(DatacenterUsageDataStore.DATABASE_URL_PROP, datacenter.path_before);
@@ -116,60 +114,22 @@ public class MergeOfResultFiles {
 
 			storeDatacenterInfo(listOfAllDatacenterInfo);
 
-
 		} else if (parsedCommand.equals("conclude")) {
+
 			double time = Double.parseDouble(conclude.time);
 
-			properties.setProperty(TaskDataStore.DATABASE_URL_PROP,
-					conclude.path_before);
+			properties.setProperty(TaskDataStore.DATABASE_URL_PROP, conclude.path_before);
 			TaskDataStore dataStore = new TaskDataStore(properties);
-			List<TaskState> tasks_before = dataStore
-					.getTasksFinishedBefore(time);
+			List<TaskState> tasks_before = dataStore.getTasksFinishedBefore(time);
 
 			System.out
 					.println("Number os tasks before: " + tasks_before.size());
-			properties.setProperty(PreemptableVmDataStore.CHECKPOINT_URL,
-					conclude.path_after);
+			properties.setProperty(PreemptableVmDataStore.CHECKPOINT_URL, conclude.path_after);
 
-			PreemptableVmDataStore vmDataStore = new PreemptableVmDataStore(
-					properties);
-
-			List<TaskState> concludingTasks = new ArrayList<TaskState>();
-
-			System.out.println("Concluding running vms of checkpoint.");
-			// runningVm
-			List<PreemptableVm> allRunningVms = vmDataStore.getAllRunningVms();
-			System.out
-					.println("Number of running Vms: " + allRunningVms.size());
-			for (PreemptableVm runningVm : allRunningVms) {
-				TaskState taskState = new TaskState(runningVm.getId(),
-						runningVm.getMips(), runningVm.getSubmitTime(), time,
-						runningVm.getActualRuntime(time),
-						runningVm.getPriority(),
-						runningVm.getNumberOfPreemptions(),
-						runningVm.getNumberOfBackfillingChoice(),
-						runningVm.getNumberOfMigrations());
-				concludingTasks.add(taskState);
-			}
-
-			System.out.println("Concluding waiting vms of checkpoint.");
-			// waitingVm
-			List<PreemptableVm> allWaitingVms = vmDataStore.getAllWaitingVms();
-			System.out
-					.println("Number of waiting Vms: " + allWaitingVms.size());
-			for (PreemptableVm waitingVm : allWaitingVms) {
-				TaskState taskState = new TaskState(waitingVm.getId(),
-						waitingVm.getMips(), waitingVm.getSubmitTime(), time,
-						waitingVm.getActualRuntime(time),
-						waitingVm.getPriority(),
-						waitingVm.getNumberOfPreemptions(),
-						waitingVm.getNumberOfBackfillingChoice(),
-						waitingVm.getNumberOfMigrations());
-				concludingTasks.add(taskState);
-			}
-
-			System.out.println("Number of checkpoint tasks: "
-					+ concludingTasks.size());
+			PreemptableVmDataStore vmDataStore = new PreemptableVmDataStore(properties);
+			List<TaskState> concludingTasks = new ArrayList<>();
+			concludeAndAddTasksToList(time, vmDataStore, concludingTasks);
+			System.out.println("Number of checkpoint tasks: " + concludingTasks.size());
 
 			List<TaskState> listOfAllTasks = new ArrayList<>();
 			listOfAllTasks.addAll(tasks_before);
@@ -177,28 +137,58 @@ public class MergeOfResultFiles {
 
 			System.out.println("Number of all tasks: " + listOfAllTasks.size());
 
-			properties.setProperty(TaskDataStore.DATABASE_URL_PROP,
-					conclude.path_output);
+			properties.setProperty(TaskDataStore.DATABASE_URL_PROP, conclude.path_output);
 
-			// dataStore.addTaskList(listOfAllTasks);
 			storeTasks(listOfAllTasks);
-
-			// for (TaskState taskState : listOfAllTasks) {
-			// listOfAllTasks.
-			//
-			// }
-
-			// List<TaskState> final_states = dataStore.getAllTasks();
-			// System.out.println("Number of tasks in final output = " +
-			// final_states.size());
-			// printGoogleTaskStates(final_states);
-
 		}
 	}
 
-	private static void loadAndStoreUsageEntries(double minTime, double maxTime, HostUsageDataStore inputDataStore, HostUsageDataStore outputDataStore) throws SQLException, ClassNotFoundException {
+	private static void concludeAndAddTasksToList(double time, PreemptableVmDataStore vmDataStore,
+												  List<TaskState> concludingTasks) {
+
+		System.out.println("Concluding running vms of checkpoint.");
+
+		// runningVm
+		List<PreemptableVm> allRunningVms = vmDataStore.getAllRunningVms();
+		System.out.println("Number of running Vms: " + allRunningVms.size());
+
+		for (PreemptableVm runningVm : allRunningVms) {
+
+            TaskState taskState = new TaskState(runningVm.getId(),
+                    runningVm.getMips(), runningVm.getSubmitTime(), time,
+                    runningVm.getActualRuntime(time),
+                    runningVm.getPriority(),
+                    runningVm.getNumberOfPreemptions(),
+                    runningVm.getNumberOfBackfillingChoice(),
+                    runningVm.getNumberOfMigrations());
+            concludingTasks.add(taskState);
+        }
+
+		System.out.println("Concluding waiting vms of checkpoint.");
+		// waitingVm
+		List<PreemptableVm> allWaitingVms = vmDataStore.getAllWaitingVms();
+		System.out.println("Number of waiting Vms: " + allWaitingVms.size());
+
+		for (PreemptableVm waitingVm : allWaitingVms) {
+
+            TaskState taskState = new TaskState(waitingVm.getId(),
+                    waitingVm.getMips(), waitingVm.getSubmitTime(), time,
+                    waitingVm.getActualRuntime(time),
+                    waitingVm.getPriority(),
+                    waitingVm.getNumberOfPreemptions(),
+                    waitingVm.getNumberOfBackfillingChoice(),
+                    waitingVm.getNumberOfMigrations());
+            concludingTasks.add(taskState);
+        }
+	}
+
+	private static void loadAndStoreUsageEntries(double minTime, double maxTime, HostUsageDataStore inputDataStore,
+												 HostUsageDataStore outputDataStore) throws SQLException,
+																							ClassNotFoundException {
+
 		int intervalIndex = 0;
 		List<UsageEntry> loadedEntries = inputDataStore.getUsageEntryInterval(intervalIndex, INTERVAL_SIZE, minTime, maxTime);
+
 		while (loadedEntries != null){
 			storeUsageEntries(loadedEntries, outputDataStore);
             loadedEntries.clear();
@@ -207,9 +197,11 @@ public class MergeOfResultFiles {
 	}
 
 	private static void storeTasks(List<TaskState> listOfAllTasks) {
+
 		TaskDataStore dataStore = new TaskDataStore(properties);
 		int count = 0;
 		int subListSize = (int) Math.ceil(listOfAllTasks.size() * 0.02);
+
 		for (int i = 0; i < listOfAllTasks.size(); i += subListSize) {
 			System.out.println("Interval = " + count++);
 			System.out.println("Min: " + i);
@@ -222,8 +214,10 @@ public class MergeOfResultFiles {
 	}
 
 	private static void storeUsageEntries(List<UsageEntry> listOfUsageEntries, HostUsageDataStore outputDataStore) {
+
 		int count = 0;
 		int subListSize = (int) Math.ceil(listOfUsageEntries.size() * 0.02);
+
 		for (int i = 0; i < listOfUsageEntries.size(); i += subListSize) {
 			System.out.println("Interval = " + count++);
 			System.out.println("Min: " + i);
@@ -236,9 +230,11 @@ public class MergeOfResultFiles {
 	}
 
 	private static void storeDatacenterInfo(List<DatacenterInfo> listOfDatacenterInfo) {
+
 		DatacenterUsageDataStore dataStore = new DatacenterUsageDataStore(properties);
 		int count = 0;
 		int subListSize = (int) Math.ceil(listOfDatacenterInfo.size() * 0.05);
+
 		for (int i = 0; i < listOfDatacenterInfo.size(); i += subListSize) {
 			System.out.println("Interval = " + count++);
 			System.out.println("Min: " + i);
@@ -249,8 +245,6 @@ public class MergeOfResultFiles {
 			dataStore.addDatacenterInfo(subList);
 		}
 	}
-
-
 
 	private static class Command {
 		@Parameter(names = "--before", description = "path of DB before time of checkpoint")
@@ -265,64 +259,4 @@ public class MergeOfResultFiles {
 		@Parameter(names = "--output", description = "path where will be saved the new file")
 		String path_output = "";
 	}
-
-	private static <T> void storeElements(List<T> elements, DataStore dataStore){
-
-	}
-
-	public static void printGoogleTaskStates(List<TaskState> newList) {
-		int size = newList.size();
-		TaskState googleTask;
-
-		DecimalFormat dft = new DecimalFormat("###.####");
-		double totalVm0Availability = 0;
-		int count0 = 0;
-		double totalVm1Availability = 0;
-		int count1 = 0;
-		double totalVm2Availability = 0;
-		int count2 = 0;
-		int totalPreemptions = 0;
-		int totalMigrations = 0;
-
-		for (int i = 0; i < size; i++) {
-			googleTask = newList.get(i);
-
-			double vmAvailabilty = googleTask.getRuntime()
-					/ (googleTask.getFinishTime() - googleTask.getSubmitTime());
-			if (googleTask.getPriority() == 0) {
-				totalVm0Availability += vmAvailabilty;
-				count0++;
-			} else if (googleTask.getPriority() == 1) {
-				totalVm1Availability += vmAvailabilty;
-				count1++;
-			} else {
-				totalVm2Availability += vmAvailabilty;
-				count2++;
-			}
-
-			totalPreemptions += googleTask.getNumberOfPreemptions();
-			totalMigrations += googleTask.getNumberOfMigrations();
-
-		}
-
-		System.out.println("total of tasks: " + (count0 + count1 + count2));
-
-		System.out.println("========== MEAN VM AVAILABILITY (priority 0) is "
-				+ dft.format((totalVm0Availability / count0)) + " =========");
-		System.out.println("========== MEAN VM AVAILABILITY (priority 1) is "
-				+ dft.format((totalVm1Availability / count1)) + " =========");
-		System.out.println("========== MEAN VM AVAILABILITY (priority 2) is "
-				+ dft.format((totalVm2Availability / count2)) + " =========");
-
-		System.out
-				.println("========== MEAN VM AVAILABILITY is "
-						+ dft.format(((totalVm0Availability
-								+ totalVm1Availability + totalVm2Availability) / size))
-						+ " =========");
-
-		System.out.println("Total of Preemptions: " + totalPreemptions);
-		System.out.println("Total of Migrations: " + totalMigrations);
-
-	}
-
 }
