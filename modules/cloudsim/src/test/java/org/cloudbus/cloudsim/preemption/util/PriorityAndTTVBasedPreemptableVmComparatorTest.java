b0010389367aaac0ead19910d658a8ad25018110
giovannifs <giovanni@lsd.ufcg.edu.br>
2017-02-03 10:17:36 -0300

    Adding more tests for TTV preemption policy

--- modules/cloudsim/src/test/java/org/cloudbus/cloudsim/preemption/util/PriorityAndTTVBasedPreemptableVmComparatorTest.java
+++ modules/cloudsim/src/test/java/org/cloudbus/cloudsim/preemption/util/PriorityAndTTVBasedPreemptableVmComparatorTest.java
@@ -1,5 +1,13 @@
 package org.cloudbus.cloudsim.preemption.util;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import junit.framework.Assert;
 
 import org.cloudbus.cloudsim.preemption.PreemptableVm;
@@ -7,9 +15,6 @@ import org.cloudbus.cloudsim.preemption.SimulationTimeUtil;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by João Mafra on 02/02/17.
@@ -36,7 +41,8 @@ public class PriorityAndTTVBasedPreemptableVmComparatorTest {
         SimulationTimeUtil simulationTimeUtil = Mockito.mock(SimulationTimeUtil.class);
         Mockito.when(simulationTimeUtil.clock()).thenReturn(5d);
 
-        PriorityAndTTVBasedPreemptableVmComparator comparator = new PriorityAndTTVBasedPreemptableVmComparator(new HashMap<>(),simulationTimeUtil);
+		PriorityAndTTVBasedPreemptableVmComparator comparator = new PriorityAndTTVBasedPreemptableVmComparator(
+				new HashMap<Integer, Double>(), simulationTimeUtil);
 
         vm0 = new PreemptableVm(0, 1, 5, 0, submitTime, 0, runtime);
         vm1 = new PreemptableVm(1, 1, 5, 0, submitTime, 1, runtime);
@@ -121,7 +127,7 @@ public class PriorityAndTTVBasedPreemptableVmComparatorTest {
         vm0 = new PreemptableVm(0, 1, 5, 0, submitTime, 2, runtime);
         vm0.setStartExec(1);
 
-        // currentRuntime = 4, minRuntime = 2.5 (SLOTarget = 50%), TTV = -1.5
+        // currentRuntime = 4, minRuntime = 2.5 (SLOTarget = 50%), TTV = 1.5
         vm1 = new PreemptableVm(1, 1, 5, 0, submitTime, 2, runtime);
         vm1.setStartExec(1);
 
@@ -151,8 +157,136 @@ public class PriorityAndTTVBasedPreemptableVmComparatorTest {
         Assert.assertEquals(-1, comparator.compare(vm1, vm0));
 
     }
+    @Test
+    public void testSortingVMs(){
+        SimulationTimeUtil simulationTimeUtil = Mockito.mock(SimulationTimeUtil.class);
+        Mockito.when(simulationTimeUtil.clock()).thenReturn(5d);
 
+        PriorityAndTTVBasedPreemptableVmComparator comparator = new PriorityAndTTVBasedPreemptableVmComparator(sloTargets, simulationTimeUtil);
 
+        // currentRuntime = 1, minRuntime = 2.5 (SLOTarget = 50%), TTV = -1.5
+        PreemptableVm vm0 = new PreemptableVm(0, 1, 5, 0, submitTime, 2, runtime);
+        vm0.setStartExec(4);
 
+        // currentRuntime = 4, minRuntime = 2.5 (SLOTarget = 50%), TTV = 1.5
+        PreemptableVm vm1 = new PreemptableVm(1, 1, 5, 0, submitTime, 2, runtime);
+        vm1.setStartExec(1);
+        
+        // currentRuntime = 3, minRuntime = 5 (SLOTarget = 100%), TTV = -2
+        PreemptableVm vm2 = new PreemptableVm(2, 1, 5, 0, submitTime, 0, runtime);
+        vm2.setStartExec(2);
 
+        // currentRuntime = 2, minRuntime = 5 (SLOTarget = 100%), TTV = -3
+        PreemptableVm vm3 = new PreemptableVm(3, 1, 5, 0, submitTime, 0, runtime);
+        vm3.setStartExec(3);
+        
+        // currentRuntime = 5, minRuntime = 2.5 (SLOTarget = 50%), TTV = 2.5
+        PreemptableVm vm4 = new PreemptableVm(6, 1, 5, 0, submitTime, 2, runtime);
+        vm4.setStartExec(0);
+        
+        // currentRuntime = 2, minRuntime = 5 (SLOTarget = 100%), TTV = -3
+        PreemptableVm vm5 = new PreemptableVm(5, 1, 5, 0, submitTime, 0, runtime);
+        vm5.setStartExec(3);
+        
+        // currentRuntime = 4, minRuntime = 2.5 (SLOTarget = 50%), TTV = 1.5
+        PreemptableVm vm6 = new PreemptableVm(6, 1, 5, 0, submitTime, 0, runtime);
+        vm6.setStartExec(1);
+
+        List<PreemptableVm> sortedVms = new ArrayList<PreemptableVm>();
+        
+        sortedVms.add(vm0);
+        sortedVms.add(vm1);
+        sortedVms.add(vm2);
+        sortedVms.add(vm3);
+        sortedVms.add(vm4);
+        sortedVms.add(vm5);
+        sortedVms.add(vm6);
+
+        Collections.sort(sortedVms, comparator);
+        
+        Assert.assertEquals(vm3, sortedVms.get(0)); //vm3 is smaller priority, TTV and ID
+        Assert.assertEquals(vm5, sortedVms.get(1));
+        Assert.assertEquals(vm2, sortedVms.get(2));
+        Assert.assertEquals(vm6, sortedVms.get(3));
+        Assert.assertEquals(vm0, sortedVms.get(4)); //vm0 has priority 2
+        Assert.assertEquals(vm1, sortedVms.get(5));
+        Assert.assertEquals(vm4, sortedVms.get(6));
+    }   
+    
+      
+    @Test
+    public void testSortingVMs2(){
+        SimulationTimeUtil simulationTimeUtil = Mockito.mock(SimulationTimeUtil.class);
+        Mockito.when(simulationTimeUtil.clock()).thenReturn(5d);
+
+        PriorityAndTTVBasedPreemptableVmComparator comparator = new PriorityAndTTVBasedPreemptableVmComparator(sloTargets, simulationTimeUtil);
+
+        // currentRuntime = 1, minRuntime = 2.5 (SLOTarget = 50%), TTV = -1.5
+        PreemptableVm vm0 = new PreemptableVm(0, 1, 5, 0, submitTime, 2, runtime);
+        vm0.setStartExec(4);
+
+        // currentRuntime = 4, minRuntime = 2.5 (SLOTarget = 50%), TTV = 1.5
+        PreemptableVm vm1 = new PreemptableVm(1, 1, 5, 0, submitTime, 2, runtime);
+        vm1.setStartExec(1);
+        
+        // currentRuntime = 3, minRuntime = 5 (SLOTarget = 100%), TTV = -2
+        PreemptableVm vm2 = new PreemptableVm(2, 1, 5, 0, submitTime, 0, runtime);
+        vm2.setStartExec(2);
+
+        // currentRuntime = 2, minRuntime = 5 (SLOTarget = 100%), TTV = -3
+        PreemptableVm vm3 = new PreemptableVm(3, 1, 5, 0, submitTime, 0, runtime);
+        vm3.setStartExec(3);
+        
+        // currentRuntime = 5, minRuntime = 2.5 (SLOTarget = 50%), TTV = 2.5
+        PreemptableVm vm4 = new PreemptableVm(6, 1, 5, 0, submitTime, 2, runtime);
+        vm4.setStartExec(0);
+        
+        // currentRuntime = 2, minRuntime = 5 (SLOTarget = 100%), TTV = -3
+        PreemptableVm vm5 = new PreemptableVm(5, 1, 5, 0, submitTime, 0, runtime);
+        vm5.setStartExec(3);
+        
+        // currentRuntime = 4, minRuntime = 2.5 (SLOTarget = 50%), TTV = 1.5
+        PreemptableVm vm6 = new PreemptableVm(6, 1, 5, 0, submitTime, 0, runtime);
+        vm6.setStartExec(1);
+
+        SortedSet<PreemptableVm> sortedVms = new TreeSet<PreemptableVm>(comparator);
+        
+        sortedVms.add(vm0);
+        sortedVms.add(vm1);
+        sortedVms.add(vm2);
+        sortedVms.add(vm3);
+        sortedVms.add(vm4);
+        sortedVms.add(vm5);
+        sortedVms.add(vm6);
+
+        // checking and removing
+        Assert.assertEquals(vm3, sortedVms.first());
+        Assert.assertEquals(vm4, sortedVms.last());
+        
+        sortedVms.remove(vm5);
+        
+        Assert.assertEquals(vm3, sortedVms.first());
+        Assert.assertEquals(vm4, sortedVms.last());
+        
+        sortedVms.remove(vm3);
+        
+        Assert.assertEquals(vm2, sortedVms.first());
+        Assert.assertEquals(vm4, sortedVms.last());
+        
+        sortedVms.remove(vm4);
+        
+        Assert.assertEquals(vm2, sortedVms.first());
+        Assert.assertEquals(vm1, sortedVms.last());
+        
+        sortedVms.remove(vm2);
+        sortedVms.remove(vm1);
+        
+        Assert.assertEquals(vm6, sortedVms.first());
+        Assert.assertEquals(vm0, sortedVms.last());
+        
+        sortedVms.remove(vm0);
+        
+        Assert.assertEquals(vm6, sortedVms.first());
+        Assert.assertEquals(vm6, sortedVms.last());
+    }
 }
